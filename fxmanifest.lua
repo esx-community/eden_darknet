5b8aa2b05b2e37e3bf1dd273cb48adcd98b7398d
rex2630 <rex2630@gmail.com>
2020-01-17 21:44:31 +0100

    Replace __resource.lua with fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'eden_darknet'
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'config.lua',
+    'server.lua'
+}
+
+client_scripts {	
+    'config.lua',
+    'client.lua'
+}
