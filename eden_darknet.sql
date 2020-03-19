17cc22cd85219999853b0cc47b44ef284152579d
rex2630 <rex2630@gmail.com>
2020-03-20 00:18:10 +0100

    SQL fix

--- eden_darknet.sql
+++ eden_darknet.sql
@@ -1,1 +1,1 @@
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (74, 'black_chip', 'Puce cryptée', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('black_chip', 'Puce cryptÃ©e', 1, 0, 1);
