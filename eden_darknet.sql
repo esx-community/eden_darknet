cd696d58c2c59d75232e95f4cd0d7f742a808f1d
rex2630 <rex2630@gmail.com>
2020-03-20 00:19:11 +0100

    SQL weight
    Im  dumb, i forgot this 🤦‍♂

--- eden_darknet.sql
+++ eden_darknet.sql
@@ -1,1 +1,1 @@
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('black_chip', 'Puce cryptée', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('black_chip', 'Puce cryptée', 1, 0, 1);
